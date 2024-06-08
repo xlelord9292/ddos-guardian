a2aae010d3d4d2f951fd7a4e92c2ed3f5dad2275
xlelord9292 <140418730+xlelord9292@users.noreply.github.com>
2024-06-08 07:06:52 -0400

    Update setup_ddosguardian_service.sh

--- setup_ddosguardian_service.sh
+++ setup_ddosguardian_service.sh
@@ -4,18 +4,23 @@ if [ -d "/etc/ddos-guardian" ]; then
     exit 1
 fi
 
+
 mkdir /etc/ddos-guardian
 
-cd /etc/ddos-gurdian
+
+cd /etc/ddos-guardian
+
 
 git clone https://github.com/xlelord9292/ddos-guardian
+
+cd ddos-guardian
 
 npm install
 
 
-
 apt update
-apt upgrade
+apt upgrade -y
+
 
 cat <<EOF > /etc/systemd/system/guardian.service
 [Unit]
@@ -24,8 +29,8 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/node /etc/ddos-guardian/attack.js
-WorkingDirectory=/etc/ddos-guardian
+ExecStart=/usr/bin/node /etc/ddos-guardian/ddos-guardian/attack.js
+WorkingDirectory=/etc/ddos-guardian/ddos-guardian
 Restart=always
 RestartSec=3
 StandardOutput=syslog
@@ -42,6 +47,16 @@ systemctl daemon-reload
 systemctl enable guardian
 systemctl start guardian
 
+
+iptables -A INPUT -p tcp --syn -m limit --limit 1/s --limit-burst 3 -j ACCEPT
+iptables -A INPUT -p udp -m limit --limit 1/s --limit-burst 3 -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s --limit-burst 3 -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -j DROP
+
+
+iptables-save > /etc/iptables/rules.v4
+
 echo "DDoS Guardian setup complete."
 
-echo "[DDoS Guardian] Please Read Docs To Learn How To Set This Up With Nginx"
+
