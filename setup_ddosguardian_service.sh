856387d6ffbf6af7d703405ef7777d0d5f2cb8c6
xlelord9292 <140418730+xlelord9292@users.noreply.github.com>
2024-06-09 02:15:59 -0400

    Update setup_ddosguardian_service.sh

--- setup_ddosguardian_service.sh
+++ setup_ddosguardian_service.sh
@@ -81,4 +81,3 @@ cd /etc/nginx/conf.d/
 git clone https://github.com/xlelord9292/ddos-guardian-layer-7
 
 echo "DDoS Guardian setup complete."
-echo "[DDoS Guardian] Please Read Docs To Learn How To Set This Up With Nginx"
