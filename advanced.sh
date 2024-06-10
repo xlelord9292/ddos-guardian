38cfd33f3c29acf2cb602c1710280ebea799cd89
xlelord9292 <140418730+xlelord9292@users.noreply.github.com>
2024-06-09 23:49:21 -0400

    Update advanced.sh

--- advanced.sh
+++ advanced.sh
@@ -1,67 +1,52 @@
 #!/bin/bash
 
-
 sudo apt-get update
 
-
 sudo apt-get install -y iptables-persistent fail2ban unbound
-
 
 iptables -F
 iptables -X
 
 iptables -A INPUT -i lo -j ACCEPT
 
-
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
 
 iptables -A INPUT -p tcp --dport 22 -m connlimit --connlimit-above 3 -j REJECT --reject-with tcp-reset
 iptables -A INPUT -p tcp --dport 22 -m recent --name sshbrute --set
 iptables -A INPUT -p tcp --dport 22 -m recent --name sshbrute --update --seconds 300 --hitcount 4 -j DROP
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-
 iptables -A INPUT -p tcp --syn -m connlimit --connlimit-above 50 -j DROP
 
+# Allow ports 80, 8080, 443, 2022
+iptables -A INPUT -p tcp -m multiport --dports 80,8080,443,2022 -j ACCEPT
 
-iptables -A INPUT -p tcp --dport 80 -m limit --limit 25/minute --limit-burst 100 -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j DROP
+iptables -A INPUT -p tcp -m limit --limit 25/minute --limit-burst 100 -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dports 80,8080 -j DROP
 
-
-iptables -A INPUT -p tcp --dport 443 -m limit --limit 25/minute --limit-burst 100 -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -j DROP
-
+iptables -A INPUT -p tcp -m limit --limit 25/minute --limit-burst 100 -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dports 443,2022 -j DROP
 
 iptables -A INPUT -p tcp -m connlimit --connlimit-above 100 --connlimit-mask 32 --connlimit-saddr -j DROP
-
 
 iptables -A INPUT -p tcp --syn -m limit --limit 1/s --limit-burst 3 -j RETURN
 iptables -A INPUT -p tcp --syn -j DROP
 
-
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
-
 
 iptables -A INPUT -p udp --dport 53 -j DROP
 iptables -A INPUT -p tcp --dport 53 -j DROP
 
-
 iptables -A INPUT -p tcp -m limit --limit 10/sec --limit-burst 20 -j ACCEPT
 iptables -A INPUT -p udp -m limit --limit 10/sec --limit-burst 20 -j ACCEPT
 
-
 iptables -P INPUT DROP
-
 
 iptables-save > /etc/iptables/rules.v4
 
-
 sudo systemctl enable netfilter-persistent
 
-
 sudo apt-get install -y fail2ban
-
 
 cat <<EOF > /etc/fail2ban/jail.local
 [DEFAULT]
@@ -85,18 +70,15 @@ findtime = 300
 bantime = 600
 EOF
 
-
 cat <<EOF > /etc/fail2ban/filter.d/http-get-dos.conf
 [Definition]
 failregex = ^<HOST> -.*"(GET|POST).*
 ignoreregex =
 EOF
 
-
 sudo systemctl restart fail2ban
 
 sudo apt-get install -y unbound
-
 
 cat <<EOF > /etc/unbound/unbound.conf
 server:
@@ -122,7 +104,6 @@ forward-zone:
     forward-addr: 1.1.1.1
     forward-addr: 8.8.8.8
 EOF
-
 
 sudo systemctl restart unbound
 sudo systemctl enable unbound
